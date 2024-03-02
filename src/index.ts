b3b47ccbfd9498c99700367c0ed99929dd6a4665
baegteun <baegteun@gmail.com>
2024-03-02 17:54:58 +0900

    :sparkles: :: Job Service 환경 추가

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { MSGSaltBot } from "./MSGSaltBot";
 import express, { Express } from "express";
 import { client } from "./discordClient";
+import { JobService } from "./services/JobService";
+import { Job } from "./interfaces/Job";
 
-export const bot = new MSGSaltBot(client);
+
+const jobs: Job[] = [
+];
+
+const jobService = new JobService(jobs);
+
+export const bot = new MSGSaltBot(client, jobService);
 
 const app: Express = express();
 const port = 3004;
